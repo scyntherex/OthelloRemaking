82bf919085ccab314d551266833837ddcc0235ec
Rone <rone_caballero@yahoo.com>
2017-10-14 15:15:09 -0700

    Attempting to make the move function
    something's wrong with the main. its not identifying all my classes

--- OBoard.cpp
+++ OBoard.cpp
@@ -17,8 +17,56 @@ OthelloBoard::OthelloBoard()
 }*/
 
 void OthelloBoard::ApplyMove(OthelloMove *move) {
-	if (move->isMovePass()) { mPassCount++; }
-	else;
+	int countPcs = 0, rBack = move->mRow, cBack = move->mCol;
+	int r = rBack, c = cBack;
+	
+	if (move->isMovePass()) { mPassCount++; } 
+
+	else {
+		//Place move
+		mBoard[r][c] = mNextPlayer;
+		//Change the score of the game with tthat one piece
+		mValue += mNextPlayer;
+		//Now look on all 8 directions
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				//No to self
+				if (i == 0 && j == 0) { continue; } 
+				//Check borders
+				if (InBounds(r, c)) { r = rBack, c = cBack; }
+				//Checking the direction to flip
+				if (mBoard[r + i][c + j] != EMPTY) {
+					while (mBoard[r + i][c + j] == -mNextPlayer) {
+						r = r + i;
+						c = c + j;
+						countPcs++;
+					}
+					//Stop if a piece of the user is found
+					if (mBoard[r + i][c + j] == mNextPlayer
+						&& countPcs >= 1 && InBounds(r + i, c + j)) {
+						//For history of moves
+						move->AddToFlipSet(OthelloMove::FlipSet(countPcs, i, j));
+
+						while (mBoard[r + i][c + j] == mNextPlayer
+							&& countPcs >= 1 && InBounds(r + i, c + j)) {
+							r = r - i;
+							c = c - j;
+
+							mBoard[r + i][c + j] = mNextPlayer;
+							mValue += mNextPlayer * 2;
+							countPcs--;
+						}
+
+					}
+					countPcs = 0; // for resetting with mValue 
+				}
+				r = rBack, c = cBack;
+			}
+		}
+		mPassCount = 0;
+	}
+	mHistory.push_back(move);
+	mNextPlayer = -mNextPlayer; //30
 }
 
 /*
--- OthelloRemake.cpp
+++ OthelloRemake.cpp
@@ -1,19 +1,19 @@
 // OthelloRemake.cpp : Main class.
 //
 
+#include "OBoard.h"
+#include "OMove.h"
+#include "OView.h"
+#include "OExceptions.h"
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
 #include <string>
 #include <sstream>
-#include "OBoard.h"
-#include "OView.h"
-
 
 using namespace std;
 
-int main()
-{
+int main() {
 	OthelloBoard board; // the state of the game board
 	OthelloView v(&board); // a View for outputting the board via operator<<
 	string userInput; // a string to hold the user's command choice
@@ -23,13 +23,40 @@ int main()
 		cout << v << endl;
 		cout << "Enter a command:" << endl;
 		cout << "move (x,x): makes 1 move of the game." << endl;
-		cout << "undo n: undoes n amount of moves from game." << endl;
-		cout << "sv: shows the current score of the game." << endl;
-		cout << "sh: shows the previous moves used in the game." << endl;
 		cout << "quit: ends the game with current score." << endl;
 		getline(cin, userInput);
 
-		if (userInput == "quit") {
+		// move (r,c)
+		if (userInput.substr(0, 4) == "move") {
+			string moveStr = userInput.substr(5);
+			//bool goodMove = false;
+			bool goodMove = true;
+			OthelloMove *m = board.CreateMove(); //20
+
+			try {
+				*m = moveStr;
+				//for (auto itr = possMoves.begin(); itr < possMoves.end(); itr++) {
+					//OthelloMove *valid = *itr;
+					//if (*valid == *m) {
+						//goodMove = true;
+					//}
+				//}
+				if (goodMove) {
+					cout << "Applying the move " << (string)(*m) << endl;
+					board.ApplyMove(m);
+				}
+				else { //30
+					cout << "\nNAHHH Invalid Move!" << endl;
+					delete m;
+				}
+			}
+			catch (OthelloException &s) {
+				cout << "\n" << s.what() << endl;
+				delete m;
+			}
+		}
+
+		else if (userInput == "quit") {
 			OthelloMove *passCurrPlayer = board.CreateMove();
 			*passCurrPlayer = "pass";
 			board.ApplyMove(passCurrPlayer);
@@ -39,12 +66,11 @@ int main()
 			delete passCurrPlayer;
 			delete passNextPlayer;
 		}
-
 		else;
 
 	} while (!board.IsFinished());
 	
 	cout << "\nGAME OVER!!!" << endl;
-    return 0;
+	return 0;
 }
 
